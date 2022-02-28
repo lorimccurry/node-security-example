ed1072ac48f8e2ca8643550ca9981c0047d22cb0
Lori McCurry <lori.mccurry@gmail.com>
2022-02-28 17:49:35 -0600

    set up session cookie with express cookie-session

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const helmet = require('helmet');
 const passport = require('passport');
 const { Strategy } = require('passport-google-oauth20');
+const cookieSession = require('cookie-session');
 
 require('dotenv').config();
 
@@ -13,6 +14,8 @@ const PORT = 3000;
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 
 const AUTH_OPTIONS = {
@@ -28,13 +31,34 @@ function verifyCallback(accessToken, refreshToken, profile, done) {
 
 passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
+// Save the session to the cookie
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+// Read the session from the cookie
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
 const app = express();
 
-// add at the top of the app so every request can pass thru the helmet middleware
+// add at the top of the app so every request
+// can pass thru the helmet middleware
 app.use(helmet());
+
+app.use(
+  cookieSession({
+    name: 'session',
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+  })
+);
 app.use(passport.initialize());
+app.use(passport.session());
 
 function checkLoggedIn(req, res, next) {
+  //req.user
   const isLoggedIn = true; // TODO
   if (!isLoggedIn) {
     return res.status(401).json({
@@ -57,7 +81,7 @@ app.get(
   passport.authenticate('google', {
     failureRedirect: '/failure',
     successRedirect: '/',
-    session: false,
+    session: true,
   }),
   (req, res) => {
     console.log('Google called us back!');
